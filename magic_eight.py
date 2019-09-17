c77f658579b27e1ac40e2f41e5d8ec1bf1a57af2
Mariah Jacobs <mjacobs@0587383301.wireless.umich.net>
2019-09-17 11:13:30 -0400

    Correct bugs

--- magic_eight.py
+++ magic_eight.py
@@ -5,7 +5,7 @@ def get_question():
     return input ()
 
 def check_question(question_str):
-    if question_str.endswith("?"):
+    if question_str.endswith("?") or question_str == "quit":
         return True
     else:
         print ("I’m sorry, I can only answer questions.")
@@ -14,11 +14,21 @@ def check_question(question_str):
 if __name__ == "__main__":
     is_valid_question = False
     question = ""
+    magic_ball_answers = ["It is certain", "It is decidedly so.", "Without a doubt.", "Yes - definitely.",
+                          "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.",
+                          "Signs point to yes.", "Reply hazy, try again.", "Ask again later.",
+                          "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
+                          "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.",
+                          "Very doubtful."]
 
-    while question != "quit" or is_valid_question == False:
-        question = get_question()
-        if check_question(question):
-            is_valid_question = True
+    #Continue looping while user does not enter "quit"
+    while question != "quit":
 
-magic_ball_answers = ["It is certain", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.","Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.","Outlook not so good.", "Very doubtful."]
-print(random.choice(magic_ball_answers))
+        #Validate that user has entered a valid question and that question is not quit
+        while is_valid_question == False:
+            question = get_question()
+            is_valid_question = check_question(question)
+
+        if question != "quit":
+            print(random.choice(magic_ball_answers))
+            is_valid_question = False
