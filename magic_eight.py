faacd74b640a66567070531d7d3c82f93d51a727
Mariah Jacobs <mjacobs@0587383301.wireless.umich.net>
2019-09-17 10:13:48 -0400

    Validate user input

--- magic_eight.py
+++ magic_eight.py
@@ -2,5 +2,18 @@ def get_question():
     print("Ask me a question...")
     return input ()
 
+def check_question(question_str):
+    if question_str.endswith("?"):
+        return True
+    else:
+        print ("I’m sorry, I can only answer questions.")
+        return False
+
 if __name__ == "__main__":
-    question = get_question()
+    is_valid_question = False
+    question = ""
+
+    while question != "quit" or is_valid_question == False:
+        question = get_question()
+        if check_question(question):
+            is_valid_question = True
